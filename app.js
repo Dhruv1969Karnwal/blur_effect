3b1d8863535aa0a84d91a8bd45cf5dc0ca12833b
Dhruv1969Karnwal <fdhruv@gmail.com>
2022-05-31 18:53:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const background_image = document.querySelector(".background_image");
+const heading_1 = document.querySelector(".heading_1");
+
+let load =0;
+
+let int = setInterval(bluring_effect,30);    /*here 30 is in milli sec */ 
+  /*if we put load and setinterval in function the it not run directly*/
+
+function bluring_effect(){
+  load++;
+  if(load >99){
+    clearInterval(int);
+  }
+  heading_1.innerText = `${load}%`;
+  heading_1.style.opacity = scale(load,0,100,1,0);
+  background_image.style.filter = `blur(${scale(load,0,100,3,0)}rem)`
+}
+
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
